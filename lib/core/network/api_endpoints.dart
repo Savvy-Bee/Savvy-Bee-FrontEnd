41c045cc103aa40b58a070b67e5f27ca804598e8
Ife Abraham <ife04abraham@gmail.com>
2026-04-15 14:14:04 +0100

    Working on spend agent screens

--- lib/core/network/api_endpoints.dart
+++ lib/core/network/api_endpoints.dart
@@ -1,9 +1,9 @@
 class ApiEndpoints {
   /// Base URL
   /// To switch environments, change only this one value:
-  static const String baseUrl = 'https://api.mysavvybee.com'; // production
-  // static const String baseUrl =
-  //     'https://savvy-bee-backend-nodejs-express-mongodb.onrender.com'; // demo
+//   static const String baseUrl = 'https://api.mysavvybee.com'; // production
+  static const String baseUrl =
+      'https://savvy-bee-backend-nodejs-express-mongodb.onrender.com'; // demo
  
   /// Auth endpoints
   static const String register = '/auth/register';
